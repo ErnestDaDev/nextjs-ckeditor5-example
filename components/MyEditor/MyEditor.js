656ad26bb56740f3b71049aec63b9de41d8e5644
Ernest Fiifi Des-Bordes <ernestdesbordes@gmail.com>
2023-02-26 21:39:42 +0000

    update

--- components/MyEditor/MyEditor.js
+++ components/MyEditor/MyEditor.js
@@ -1,15 +1,15 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
 import React from "react";
 
 export default function MyEditor(props) {
   return (
     <CKEditor
-      editor={ClassicEditor}
+      editor={Editor}
       config={
         {
-          // input your configurations..
+          readOnly: true,
         }
       }
       data={props.data}
